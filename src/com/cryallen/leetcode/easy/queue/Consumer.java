07e2a7ef10639db8c10c5006b722554771c56d1c
Allen <cr330326@gmail.com>
2022-05-19 15:18:49 +0800

    Add: 增加队列数据结构

---
+++ src/com/cryallen/leetcode/easy/queue/Consumer.java
@@ -0,0 +1,23 @@
+package com.cryallen.leetcode.easy.queue;
+
+import java.io.IOException;
+import java.util.Random;
+
+public class Consumer {
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            BasicQueue queue = new BasicQueue("./", "task");
+            Random rnd = new Random();
+            while (true) {
+                byte[] bytes = queue.dequeue();
+                if (bytes == null) {
+                    Thread.sleep(rnd.nextInt(1000));
+                    continue;
+                }
+                System.out.println("consume: " + new String(bytes, "UTF-8"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
